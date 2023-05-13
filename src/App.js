9be2279c28a420fd28526642c559843eba0f4c32
kiranwankhade <kiranwankhade7738@gamil.com>
2023-05-13 17:37:03 +0530

    final

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import AllRoutes from "./Components/AllRoutes";
 import Navbar1 from './Components/Navbar1';
 import Navbar2 from './Components/Navbar2';
 import Footer1 from './Components/Footer1';
-import Footer2 from './Components/Footer2';
 
 
 
